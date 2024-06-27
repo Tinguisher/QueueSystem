abf99954033b7f9a5059ea4e906fa4683277dab1
unknown <knnthbinay@gmail.com>
2024-06-27 17:04:46 +0800

    debug post

--- SignupHandle.php
+++ SignupHandle.php
@@ -1,9 +1,31 @@
 <?php
 
-echo 'Why press me';
+echo 'Why press me <br>';
 
-if($_SERVER["REQUEST_METHOD"] !== "POST"){
-    header("Location: NotFound.html");
+// if the file is accessed manually and not post
+if ($_SERVER["REQUEST_METHOD"] !== "POST"){
+    header("Location: ./NotFound.html");
+    exit;
 }
+
+// TO DOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+// if there is input of data
+if ( !empty ($_POST) ){
+    // access database
+    $mysqli = require "./database.php";
+
+    // get post values with password as hashed
+    $name = $_POST["input_name"];
+    $email = $_POST["input_email"];
+    $password = hash('sha256', $_POST["input_password"]);
+
+    // sql query to create new user
+    $sql = "INSERT INTO `user`(`email`, `password`, `name`) VALUES ('". $email ."','". $password ."','". $name ."');";
+
+    // run query to website
+    $mysqli->query($sql);
+}
+
+
 
 ?>
