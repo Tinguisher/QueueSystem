cddb0f10b477edba35d03a919a12241c6ac4dbd1
unknown <knnthbinay@gmail.com>
2024-07-02 10:19:52 +0800

    request php

--- SignupHandle.php
+++ SignupHandle.php
@@ -1,6 +1,6 @@
 <?php
-
-echo 'Why press me <br>';
+// all contents to read as json
+header('Content-Type: application/json; charset=utf-8');
 
 // if the file is accessed manually and not post
 if ($_SERVER["REQUEST_METHOD"] !== "POST"){
@@ -8,24 +8,48 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST"){
     exit;
 }
 
-// TO DOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
-// if there is input of data
-if ( !empty ($_POST) ){
-    // access database
-    $mysqli = require "./database.php";
+// get raw input from the form instead of json
+$json = file_get_contents('php://input');
+$data = json_decode($json, true);
 
-    // get post values with password as hashed
-    $name = $_POST["input_name"];
-    $email = $_POST["input_email"];
-    $password = hash('sha256', $_POST["input_password"]);
-
-    // sql query to create new user
-    $sql = "INSERT INTO `user`(`email`, `password`, `name`) VALUES ('". $email ."','". $password ."','". $name ."');";
-
-    // run query to website
-    $mysqli->query($sql);
+// check if there is input of data
+if ( empty($data['input_name']) || empty($data['input_email'])  || empty($data['input_password']) ){
+    $response = [
+        'status' => "error",
+        'message' => "All fields are required"
+    ];
+    exit( json_encode($response) );
 }
 
+// access database
+$mysqli = require "./database.php";
 
+// get post values with password as hashed
+$name = $data['input_name'];
+$email = $data['input_email'];
+$password = hash('sha256', $data['input_password']);
+
+// sql query to create new user
+$sql = "INSERT INTO `user`(`email`, `password`, `name`) VALUES ('". $email ."','". $password ."','". $name ."');";
+
+// try the query
+try{
+    $mysqli->query($sql);
+    $response = [
+        'status' => "success",
+        'message' => "Signup Successful"
+    ];
+}
+
+// if there is error in query
+catch (Exception $e){
+    $response = [
+        'status' => "error",
+        'message' => "Error No: ".$e->getCode(). " - ". $e->getMessage()    // get error code and message
+    ];
+}
+
+// output the response
+exit ( json_encode($response) );
 
 ?>
