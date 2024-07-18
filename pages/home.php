c442dab8bc19253db1dc61dd8f6ddef899b70cc3
unknown <knnthbinay@gmail.com>
2024-07-18 15:56:18 +0800

    do css home

--- pages/home.php
+++ pages/home.php
@@ -127,41 +127,50 @@ if ( isset($_SESSION['id']) ){
         </div>
     
         <div class="slider">
-            <div class="slides">
-              
-              <input type="radio" name="radio-btn" id="radio1">
-              <input type="radio" name="radio-btn" id="radio2">
-              <input type="radio" name="radio-btn" id="radio3">
-              <input type="radio" name="radio-btn" id="radio4">
 
-              <div class="slide first">
-                <img src="../images/g1.png">
-              </div>
-              <div class="slide">
-                <img src="../images/g2.png">
-              </div>
-              <div class="slide">
-                <img src="../images/g3.png">
-              </div>
-              <div class="slide">
-                <img src="../images/g4.png">
-              </div>
-              <div class="navigation-auto">
-                <div class="auto-btn1"></div>
-                <div class="auto-btn2"></div>
-                <div class="auto-btn3"></div>
-                <div class="auto-btn4"></div>
-              </div>
-            </div>
             
-            <div class="navigation-manual">
-              <label for="radio1" class="manual-btn"></label>
-              <label for="radio2" class="manual-btn"></label>
-              <label for="radio3" class="manual-btn"></label>
-              <label for="radio4" class="manual-btn"></label>
-            </div>
             
-          </div>
+            <div id="slides">
+                <!-- <h4>LOADING SLIDES...</h4> -->
+                
+                <input type="radio" name="radio-btn" id="radio1">
+                <input type="radio" name="radio-btn" id="radio2">
+                <input type="radio" name="radio-btn" id="radio3">
+                <input type="radio" name="radio-btn" id="radio4">
+                
+
+                <!-- <div class="slide first">
+                    <img src="../images/g1.png">
+                </div>
+                <div class="slide">
+                    <img src="../images/g2.png">
+                </div>
+                <div class="slide">
+                    <img src="../images/g3.png">
+                </div>
+                <div class="slide">
+                    <img src="../images/g4.png">
+                </div> -->
+
+                <div id="navigation-auto">
+                    <div class="auto-btn1"></div>
+                    <div class="auto-btn2"></div>
+                    <div class="auto-btn3"></div>
+                    <div class="auto-btn4"></div>
+                </div>
+                <div id="navigation-manual">
+                    <!-- <h4>LOADING NAVIGATION MANUAL...</h4> -->
+                    <label for="radio1" class="manual-btn"></label>
+                    <label for="radio2" class="manual-btn"></label>
+                    <label for="radio3" class="manual-btn"></label>
+                    <label for="radio4" class="manual-btn"></label>
+                </div>
+                
+            </div>
+                
+            
+            
+        </div>
 
         <div>
             <p id="home">Home</p>
