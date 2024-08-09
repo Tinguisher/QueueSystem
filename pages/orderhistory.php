1ba9cb5713f64b7933a4e46e28de84a1f571d3b1
chesca-dm <chesca27.cdm@gmail.com>
2024-08-09 16:25:44 +0800

    orderhistory

--- pages/orderhistory.php
+++ pages/orderhistory.php
@@ -13,7 +13,7 @@ $mysqli->close();
 <head>
 	<meta charset="utf-8" />
 	<title>Snap Serve</title>
-	<link rel="stylesheet" href="../stylesheets/summary.css">
+	<link rel="stylesheet" href="../stylesheets/orderhistory.css">
 	<link href='https://fonts.googleapis.com/css?family=Averia Serif Libre' rel='stylesheet'>
 	<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer">
